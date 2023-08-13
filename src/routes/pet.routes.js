222550cdce17fe1c17c09e15cabc3b0e69d10d60
DanielCamposDevX <daniel.campos.dev3@gmail.com>
2023-08-13 20:10:15 -0300

    add/Route

--- src/routes/pet.routes.js
+++ src/routes/pet.routes.js
@@ -1,11 +1,12 @@
 import { Router } from "express"
-import { catModel, getBreeds, getCat } from "../controllers/pet.controller.js";
+import { catModel, getBreeds, getCat, postCat } from "../controllers/pet.controller.js";
 
 const petRouter = Router();
 
 petRouter.get('/cats', getCat);
 petRouter.get('/cats/:id', catModel);
 petRouter.get('/breed/:id', getBreeds);
+petRouter.post('/cats', postCat);
 
 
 
