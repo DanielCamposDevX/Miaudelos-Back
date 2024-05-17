fc83137d7a2ca4d5dbbf293dfe9313a7893408da
Daniel Campos <daniel.campos@devio.com.br>
2024-05-17 11:12:40 -0300

    fix(routes location)

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
-import express from "express";
 import cors from "cors";
-import Routes from "./src/routes/index.routes.js";
+import express from "express";
+import Routes from "./routes/index.routes.js";
 
 const app = express();
 app.use(express.json());
